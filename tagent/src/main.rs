f27f7e0e55f390788216cabb9e4940010aa7f42b
Joe Stubbs <joestubbs5@gmail.com>
2022-02-19 09:30:57 -0600

    Log the root_dir used at start up.

--- tagent/src/main.rs
+++ tagent/src/main.rs
@@ -2,6 +2,7 @@ use actix_web::middleware::Logger;
 use actix_web::web::ServiceConfig;
 use actix_web::{web, App, HttpServer};
 use dotenv::dotenv;
+use log::info;
 
 mod auth;
 mod config;
@@ -36,6 +37,7 @@ async fn main() -> std::io::Result<()> {
 
     let app_version = String::from("0.1.0");
     let root_dir = handlers::get_root_dir()?;
+    info!("tagent running with root directory: {}", root_dir);
     // let pub_key = String::from("-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAieWoWm/7AZPheleSJSXR/CS9vnr2m7qsjzvqv7PXr5AOxpw+eYn5h1/7lBqludzle4fI8ai/mv2WsTEC7C3HuIF5D+EuCQtXe89YPI8e4Q/gc660vWhG3ZYA5UrZyAOAJvDvcd/N8ZCjyW8fZ5tYsMlcWf6m9d29QLtLc8kIIZJiFuQcfiq5NiaB5tYU6zOQzO6fYUO44egni1DH6spm0btqobIsNQauunXSuZD3lLwXGnuS1VE+3pPEIFeAq0tnQcuJxsUZIRbiWRgAnNHCFoxeB3kMysKUr1IMjqlUlTBgDbCvfn8RJxQUeMgEJygsa/m9xHzfX3IoAm4NfvsEPwIDAQAB\n-----END PUBLIC KEY-----");
     let pub_key = config::get_pub_key().await?;
     let app_state = representations::AppState {
