d42f3f6de9c494085d54848144a2d16db700a739
Joe Stubbs <joestubbs5@gmail.com>
2022-04-02 10:40:18 -0500

    this commit adds a custom error handler that will wrap an actix-generated error and return JSON.

--- tagent/src/main.rs
+++ tagent/src/main.rs
@@ -1,8 +1,6 @@
-use actix_web::middleware::{Logger, ErrorHandlerResponse, ErrorHandlers};
-use actix_web::web::{JsonConfig, ServiceConfig, PathConfig};
-use actix_web::http::StatusCode;
-use actix_web::{web, dev, App, HttpServer, Result};
-use actix_web::{error, HttpResponse};
+use actix_web::middleware::Logger;
+use actix_web::web::{JsonConfig, PathConfig, ServiceConfig};
+use actix_web::{web, App, HttpServer};
 
 #[macro_use]
 extern crate diesel;
@@ -38,27 +36,10 @@ fn make_config(app_data: web::Data<representations::AppState>) -> impl FnOnce(&m
                 .service(handlers::list_files_path)
                 .service(handlers::get_file_contents_path)
                 .service(handlers::post_file_contents_path)
-                .default_service(web::route().to(handlers::not_found))
+                .default_service(web::route().to(handlers::not_found)),
         );
     }
 }
-
-
-fn make_error_json_rsp<B>(mut res: dev::ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> 
-where B: std::fmt::Debug + actix_web::body::MessageBody {
-
-    let (req, res) = res.into_parts();
-    let msg = res.body().clone();
-    let body = format!("{{\"status\": \"error\", \"message\": {:#?}, \"result\": \"none\"}}", msg);
-    // let res = res.set_body(r#"{"status": "error", "message": "", "result": "none"}"#.to_owned());
-    let res = res.set_body(body);
-    let res = dev::ServiceResponse::new(req, res)
-        .map_into_boxed_body()
-        .map_into_right_body();
-
-    Ok(ErrorHandlerResponse::Response(res))
-}
-
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -67,18 +48,18 @@ async fn main() -> std::io::Result<()> {
 
     // custom `Json` extractor configuration
     let json_cfg = JsonConfig::default()
-    // limit request payload size
-    .limit(4096*1024)
-    // use custom error handler
-    .error_handler(|err, req| {
-        representations::TagentError::new_with_version(err.to_string()).into()
-    });
+        // limit request payload size
+        .limit(4096 * 1024)
+        // use custom error handler
+        .error_handler(|err, _req| {
+            representations::TagentError::new_with_version(err.to_string()).into()
+        });
 
     let path_cfg = PathConfig::default()
-    // use custom error handler
-    .error_handler(|err, req| {
-        representations::TagentError::new_with_version(err.to_string()).into()
-    });
+        // use custom error handler
+        .error_handler(|err, _req| {
+            representations::TagentError::new_with_version(err.to_string()).into()
+        });
 
     let settings = crate::config::TagentConfig::from_sources()?;
     let app_version = String::from(env!("CARGO_PKG_VERSION"));
@@ -105,7 +86,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(Logger::default())
             .wrap(Logger::new("%a %{User-Agent}i"))
             .configure(make_config(actix_app_state.clone()))
-            // .default_service(handlers::not_found)
+        // .default_service(handlers::not_found)
     })
     .bind(format!("{}:{}", settings.address, settings.port))?
     .run()
